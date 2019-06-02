d64e358e7328e64b90f66b54226fccc4cfda5471
mmasima <mmasima@student.wethinkcode.co.za>
2019-06-02 23:43:51 +0200

    ft_strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -12,19 +12,40 @@
 #include <stdio.h>
 #include "libft.h"
 
+char *ft_strstr( char *haystack,  char *needle)
+{
+	int len;
+	int y;
 
+	len = 0;
+	y = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[len] != '\0')
+	{
+		if (haystack[len] == needle[0])
+		{
+			y = 1;
+			while ( needle[y] != '\0' && haystack[len + y] == needle[y])
+				y++;
+			if(needle[y] == '\0')
+				return ((char*)&haystack[len]);
 
-
+		}
+		len++;
+	}
+	return (0);
+}
 
 int main()
 {
     // Take any two strings
     char s1[] = "GeeksforGeeks";
-    char s2[] = "for";
+    char s2[] = "e";
     char *p;
 
     // Find first occurence of s2 in s1
-    p = strstr(s1, s2);
+    p = ft_strstr(s1, s2);
 
     // Prints the result
     if (p) {
