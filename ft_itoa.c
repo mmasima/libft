fe49c71733cd1116887c2a7fff848a3484d71a43
Morapedi MASIMA <mmasima@c4r8s2.wethinkcode.co.za>
2019-06-13 14:21:09 +0200

    work

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,38 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmasima <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/11 10:37:48 by mmasima           #+#    #+#             */
-/*   Updated: 2019/06/12 13:11:32 by mmasima          ###   ########.fr       */
+/*   Created: 2019/06/13 11:20:10 by mmasima           #+#    #+#             */
+/*   Updated: 2019/06/13 12:30:56 by mmasima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_itoa(int n)
 {
-	int		x;
-	int		y;
 	char	*str;
 
-	x = 0;
-	y = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (str == NULL)
+	if (!(str = (char *)malloc(sizeof(char) * 2)))
 		return (NULL);
-	while (s1[x] != '\0')
+	if (n == -2147483648)
+		return (ft_strcpy(str, "-2147483648"));
+	if (n < 0)
 	{
-		str[x] = s1[x];
-		x++;
+		str[0] = '-';
+		str[1] = '\0';
+		str = ft_strjoin(str, ft_itoa(-n));
 	}
-	while (s2[y] != '\0')
+	else if (n >= 10)
+		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
+	else if (n < 10 && n >= 0)
 	{
-		str[x + y] = s2[y];
-		y++;
+		str[0] = n + '0';
+		str[1] = '\0';
 	}
-	str[x + y] = '\0';
 	return (str);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strnequ.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmasima <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/11 10:37:48 by mmasima           #+#    #+#             */
-/*   Updated: 2019/06/12 13:11:32 by mmasima          ###   ########.fr       */
+/*   Created: 2019/06/12 13:13:55 by mmasima           #+#    #+#             */
+/*   Updated: 2019/06/12 13:49:50 by mmasima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	int		x;
-	int		y;
-	char	*str;
-
-	x = 0;
-	y = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (str == NULL)
-		return (NULL);
-	while (s1[x] != '\0')
-	{
-		str[x] = s1[x];
-		x++;
-	}
-	while (s2[y] != '\0')
-	{
-		str[x + y] = s2[y];
-		y++;
-	}
-	str[x + y] = '\0';
-	return (str);
+	return (ft_strncmp(s1, s2, n) ? 0 : 1);
 }
