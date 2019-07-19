3c73b575f058d3619a5616297681b16bf75570d5
Morapedi MASIMA <mmasima@c5r6s1.wethinkcode.co.za>
2019-07-19 14:45:55 +0200

    updated memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: mmasima <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 16:52:12 by mmasima           #+#    #+#             */
-/*   Updated: 2019/06/10 10:40:06 by mmasima          ###   ########.fr       */
+/*   Updated: 2019/07/19 14:45:10 by mmasima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	*ft_memalloc(size_t size)
 {
-	void	*mem;
+	char	*ptr;
+	size_t	i;
 
-	mem = (unsigned char *)malloc(size);
-	if (mem == NULL)
+	if (!(ptr = (char*)malloc(sizeof(char) * size)))
 		return (NULL);
-	ft_memset(mem, 0, size);
-	return (mem);
+	i = -1;
+	while (++i < size)
+		*(ptr + i) = 0;
+	return (ptr);
 }
+
